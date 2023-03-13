30dd391cbb2ef1dbbb5653993247204a9a2fe71d
Andrey Lebedev <dreamapps.agency@gmail.com>
2023-03-13 14:45:39 +0400

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Andrey Lebedev on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
